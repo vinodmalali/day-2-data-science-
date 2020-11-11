57c362469c48ddf91cd7c14e9fa44dc2fbcccffb
vinodmalali <70831777+vinodmalali@users.noreply.github.com>
2020-11-11 22:31:48 +0530

    Add files via upload

---
+++ day 2. (1).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+1)
+
+# In[13]:
+
+
+l=[]
+i=int(input("enter the value\n"))
+if i%2==0:
+       l.append(i)
+print(l) 
+
+3)
+# In[10]:
+
+
+d=dict()
+x=int(input("enter the number"))
+for i in range(1,x+1):
+    d[i]=i**2
+print(d)
+
+2)
+# In[15]:
+
+
+lst1=[1,2,3,4]
+lst2=[1,2,3,5]
+print "comparision of lst2 with lst1 : ",
+print cmp(lst1,lst2)
+
+
+# In[ ]:
+
+
+
+
